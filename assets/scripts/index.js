55d54548d1dea178abc2938528ed288f1711a2e8
alex-braun <braunacb@gmail.com>
2016-07-23 20:47:10 -0400

    Created main index file for JS to output
    The index.js file to bundle all other JS.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,4 +4,9 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+const authEvents = require('./auth/events');
+
+// On document ready
+$(() => {
+  authEvents.addHandlers();
+});
