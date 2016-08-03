61adb41197def8a15bc6776718769e4e9479c2f1
alex-braun <braunacb@gmail.com>
2016-08-03 14:19:40 -0400

    Deploying game to gh pages.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(28);
+	module.exports = __webpack_require__(27);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 28:
+/***/ 27:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
